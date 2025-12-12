d75935ef3205b1e4d0342321e057b39c6a398010
Adarsh01433 <adarshtiwarip1433@gmail.com>
2025-12-12 14:35:28 +0530

    Add auth controller and refresh token logic

--- controller/auth/auth.js
+++ controller/auth/auth.js
@@ -89,11 +89,82 @@ const login = async (req, res)=> {
    
 }
 
- const refreshToken = async (req, res)=> {
-   const {refresh_token, type} = req.body; 
-      if(!type || !["scoket", "app"].includes(type) || !refresh_token){
-         throw new BadRequestError("Invalid body")
+const refreshToken = async(req, res)=> {
+   const {type, refresh_token} = req.body
+   if(!type || !["socket", "app"].includes(type) || !refresh_token){
+    throw new BadRequestError("Invalid body");
+   }
+
+   try {
+     let accessToken , newRefreshToken;
+     if(type === "app"){
+     ({ accessToken, newRefreshToken} = await generateRefreshToken(
+      refresh_token,
+      process.env.REFRESH_TOKEN_SECRET,
+      process.env.REGISTER_SOCKET_TOKEN_EXPIRY,
+      process.env.JWT_SECRET,
+      process.env.ACCESS_TOKEN_EXPIRY
+     ))
+     } else if (type === "socket"){
+      ({ accessToken, newRefreshToken} = await generateRefreshToken(
+        refresh_token,
+        process.env.REFRESH_SOCKET_TOKEN_SECRET,
+        process.env.REFRESH_SOCKET_TOKEN_EXPIRY,
+        process.env.SOCKET_TOKEN_SECRET,
+        process.env.SOCKET_TOKEN_EXPIRY
+      ))
+     }
+     res.status(StatusCodes.OK).json({access_token : accessToken, refresh_token : newRefreshToken})
+   } catch (error) {
+     console.error(error);
+     throw new UnautheticatedError("Invalid Token")
+   }
+}
+
+ // ------- Helper Function -------
+ async function generateRefreshToken(
+      token,
+      refresh_secret,
+      refresh_expiry,
+      access_secret,
+      access_expiry
+ ) {
+      try {
+        const payload = jwt.verify(token, refresh_secret);
+        const user = await User.findById(payload.userId);
+        if(!user){
+          throw new NotFoundError("User not found");
+        }
+        const access_token = jwt.sign(
+          {userId : payload.userId},
+          access_secret,
+          {expiresIn : access_expiry}
+        );
+        const newRefreshToken = jwt.sign(
+          {userId : payload.userId},
+          refresh_secret,
+          {expiresIn : refresh_expiry}
+        )
+        return {access_token , newRefreshToken};
+        
+      } catch (error) {
+        console.error(error);
+        throw new UnautheticatedError("Invalid Token")
+        
       }
  }
+ 
+
+ // ----- LogOut -------
+  const logout = async (req, res)=> {
+   const accessToken = req.headers.authorization.split(" ")[1];
+   const decodedToken = jwt.decode(accessToken, process.env.JWT_SECRET);
+   const userId = decodedToken.userId;
+   await User.updateOne(({_id : userId}, {$unset : {biometricKey : 1} }));
+   res.status(StatusCodes.OK).json({message : "Logged out sucessfully"});
+  };
+
+  export {register, login, refreshToken, logout};
+
 
 
