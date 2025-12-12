67caca587b6e9905d4495312182cae43e3829f55
Adarsh01433 <adarshtiwarip1433@gmail.com>
2025-12-12 15:40:03 +0530

    completed some middleware and register to app.js

---
+++ middleware/authentication.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import {UnautheticatedError} from  "../errors/index.js"
+
+const auth = async(req, res, next)=> {
+ const authHeader = req.headers.authorization;
+ if(!authHeader || ! authHeader.startWith("Bearer ")){
+    throw new UnautheticatedError("Authentication invalid");
+ }
+
+ const token = authHeader.split(" ")[1];
+
+ try {
+      const payload = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = {userId : payload.userId, name : payload.name};
+      next();
+ } catch (error) {
+    throw new UnautheticatedError("Authentocation invalid")
+ }
+}
+
+export default auth
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,12 @@
 import express from "express";
-
+import {login, logout, refreshToken, register} from "../controller/auth/auth.js"
 const router = express.Router();
+import authenticateUser from "../middleware/authentication.js"
 
+router.post("/refresh-token", refreshToken);
+router.post("/logout", authenticateUser, logout);
+router.post("/register", register);
+router.post("/login", login);
+
+
+export default router;
